fde31b9a2e4eb243c24527c7af628e348e26db0f
Francisco Neto <bezerra.neto@academico.ifpb.edu.br>
2022-03-12 18:03:10 -0300

    Implementação completa
    Implementação terminada, ainda falta a interação com o usuário, mas já dá pra usar o código do jeito que o for útil.
    /*--------------------------------------*/
    
    Implementation finished, there is still a lack of interaction with the user, but you can use the code in any way that is useful.

--- Listas encadeadas/linkedList.c
+++ Listas encadeadas/linkedList.c
@@ -110,54 +110,187 @@ int lenLinkedList(node* list)
 
 int elementIndex(node* list, int element)
 {
+    // index do elemento
     int index = 0;
+    // copia da lista
     node* cpyList = list;
 
-    while (cpyList->data != element)
+    // enquanto cpyList nao for nulo
+    while (cpyList != NULL)
     {
+        // se o dado atual for o elemento buscado
+        // retorna o indice
+        if (cpyList->data == element) return index;
+
+        // caso contrario
+        // indece eh incrementado
         index++;
+
+        // cpyList eh iterado
         cpyList = cpyList->next;
     }
 
-    return index;
+    // retorna -1 em caso de erro
+    return -1;
 }
 
 int removeInIndex(node** list, int index)
 {
-    node* aux = *list, *prev = NULL;
+    // auxiliar eh instanciado
+    node* aux = *list;
+    // contador para verificar o indice
     int countIndex = 0;
 
+    // se a lista estiver vazia e o indice nao for 0
+    // retorna 0
     if (emptyList(*list) && index != 0) return 0;
 
+    // se o indice for maior que o possivel
+    // retorna 0
     if (index > lenLinkedList(*list)) return 0;
 
     switch (index)
     {
+        // primeiro indice
         case 0:
+            // posicao atual recebe a proxima
             *list = (*list)->next;
-            return 
+            return 1;
+        // ultima posicao
         case -1:
-            while (aux->next != NULL)
+            // enquanto o item analizado nao for o prnultimo
+            while (aux->next->next != NULL)
             {
-                prev = aux;
+                // auxiliar anda uma casa
                 aux = aux->next;
             }
 
-            prev->next = aux->next;
-            (*list)->next = prev;
-            free(aux);
-            break;
+            // o proximo item recebe o ultimo
+            aux->next = aux->next->next;
+            return 1;
+        // qualquer outra posicao
         default:
-            while (countIndex != index)
+            // enquanto o contador nao estiver antes do indice
+            while (countIndex != index-1)
                 {
+                    // contador eh incrementado
                     countIndex++;
-                    prev = aux;
+                    // auxiliar eh incrementado
                     aux = aux->next;
                 }
 
-            prev->next = aux->next;
-            free(aux);
-            *list = prev;
-            break;
+            // o proximo item recebe o seu proximo
+            aux->next = aux->next->next;
+            return 1;
     }
+}
+
+int removeItem(node** list, int data)
+{
+    // se o elemento nao for encontrado ele retorna 0
+    if (elementIndex(*list, data) < 0) return 0;
+
+    // auxiiliar eh instanciado
+    node* aux = *list;
+
+    // se o dado atual for igual ao dado inserido
+    // sigfica que o item eh o primeiro
+    if (aux->data == data)
+    {
+        // entao eh so fazer o atual recber o valor do proximo
+        *list = (*list)->next;
+        return 1;
+    }
+
+    // enquanto o dado for diferente
+    while (aux->next->data != data)
+    {
+        // auxiliar eh atualizado
+        aux = aux->next;
+    }
+
+    // dado eh removido
+    aux->next = aux->next->next;
+
+    return 1;
+}
+
+int elementInIndex(node* list, int index)
+{
+    // verifica se o indice eh possivel
+    if (index > lenLinkedList(list) - 1) return 0;
+
+    // para i enquanto for menor que o indice
+    for (int i = 0; i < index; i++)
+    {
+        // lista eh atualizada
+        list = list->next;
+    }
+
+    // retorna o dado do indice
+    return list->data;
+}
+
+int equalList(node* listOne, node* listTwo)
+{
+    // tamanho das listas
+    int sizeOne = lenLinkedList(listOne);
+    int sizeTwo = lenLinkedList(listTwo);
+
+    // se os tamanhos forem diferentes
+    // retorna -1
+    if (sizeOne != sizeTwo) return -1;
+
+    // enquanto o tamanho nao for 0
+    while (sizeOne > 0)
+    {
+        // se o o dado entre elas forem diferentes
+        if (listOne->data != listTwo->data)
+        {
+            // retorna 0
+            return 0;
+        }
+        sizeOne--;
+    }
+
+    // retorna 1 indicando sucesso
+    return 1;
+}
+
+int fullToEmpty(node** list)
+{
+    // se a lista estiver vazia retorna 0
+    if (emptyList(*list)) return 0;
+
+    // enquanto a lista nao for nula
+    while (*list != NULL)
+    {
+        // lista eh atualaizada
+        *list = (*list)->next;
+    }
+
+    // retorna 1
+    return 1;
+}
+
+int listCpy(node** source, node** destination)
+{
+    // se o destino nao estiiver vazio
+    // ele sera esvaziado
+    if (!emptyList(*destination)) fullToEmpty(destination);
+
+    // enquanto a atribuicao for possivel
+    while (*destination = *source)
+    {   
+        // as listas "andarao" uma casa
+        *source = (*source)->next;
+        *destination = (*destination)->next;
+    }
+
+    // se as listas foram diferentes
+    // retorna 0
+    if (!equalList(*source, *destination)) return 0;
+
+    // retorna um indicando sucesso
+    return 1;
 }
--- Listas encadeadas/linkedList.h
+++ Listas encadeadas/linkedList.h
@@ -19,16 +19,39 @@ typedef struct linkedList
     struct linkedList* next;
 }node;
 
+// inicializa a lista
 void startList(node** list);
 
+// verifica se a lista esta vazia
 int emptyList(node* list);
 
+// insere de acordo com o indice
 int insertInIndex(node** list, int data, int index);
 
+// printa a lista
 void printList(node* list);
 
+// retorna o tamanho da lista
 int lenLinkedList(node* list);
 
+// retorna o indice do elemento
 int elementIndex(node* list, int item);
 
+// remove o elemento de acordo com o indice
 int removeInIndex(node** list, int index);
+
+// remove o item de acordo com o valor
+int removeItem(node** list, int data);
+
+// retorna o elemento no indice inserido
+int elementInIndex(node* list, int index);
+
+// compara listas
+int equalList(node* listOne, node* listTwo);
+
+// esvazia as listas
+int fullToEmpty(node** list);
+
+// copia uma lista em outra
+int listCpy(node** source, node** destination);
+
